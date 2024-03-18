bc44d1522665a739fc4b85968ed33792b37564df
Tyler <tystojack@gmail.com>
2024-03-18 07:45:00 -0500

    spinning dice

--- src/components/ThreeD.jsx
+++ src/components/ThreeD.jsx
@@ -1,48 +1,62 @@
 import React, { useRef, useState } from "react";
 import { Canvas, useFrame } from "@react-three/fiber";
+import Cylinder from "../components/Cylinder"
 
 function Dice(props) {
   const meshRef = useRef();
   const [hovered, setHover] = useState(false);
   const [active, setActive] = useState(false);
 
-  // useFrame((state, delta) => (meshRef.current.rotation.x += delta * 0.3));
+  useFrame((state, delta) => {
+      meshRef.current.rotation.y += delta ;
+    meshRef.current.rotation.x += delta * 2;
+  });
 
   return (
     <mesh
-      {...props}
-      ref={meshRef}
-      scale={active ? 1.5 : 1}
-      onClick={(event) => setActive(!active)}
-      onPointerOver={(event) => setHover(true)}
-      onPointerOut={(event) => setHover(false)}
-    >
-      <boxGeometry args={[2, 2, 2]} />
-      <meshStandardMaterial color={hovered ? "gray" : "#bf0a0a"} />
-      {[...Array(6)].map((_, index) => (
-        <Dot key={index} position={getPosition(index)} />
-      ))}
-    </mesh>
+    {...props}
+    ref={meshRef}
+    scale={active ? 1.5 : 1}
+    onClick={(event) => setActive(!active)}
+    onPointerOver={(event) => setHover(true)}
+    onPointerOut={(event) => setHover(false)}
+  
+  >
+    <boxGeometry args={[2, 2, 2]} />
+    <meshStandardMaterial color={hovered ? "gray" : "#bf0a0a"} />
+    {[...Array(21)].map((_, index) => {
+      const { position, rotation } = getPosition(index);
+      return <Cylinder key={index} position={position} rotation={rotation} />;
+    })}
+  </mesh>
   );
 }
 
-function Dot({ position }) {
-  return (
-    <mesh position={position}>
-      <sphereGeometry args={[0.16, 32, 32]} />
-      <meshStandardMaterial color="white" />
-    </mesh>
-  );
-}
+
 
 function getPosition(index) {
   const positions = [
-    [0, 1, 0],
-    [0, -1, 0],
-    [1, 0, 0],
-    [-1, 0, 0],
-    [0, 0, 1],
-    [0, 0, -1],
+    { position: [0.6, 1, 0.6], rotation: [0, 0, 0] }, // Top 
+    { position: [0.6, 1, 0], rotation: [0, 0, 0] }, // Top 
+    { position: [-0.6, 1, 0], rotation: [0, 0, 0] }, // Top 
+    { position: [-0.6, 1, 0.6], rotation: [0, 0, 0] }, // Top 
+    { position: [0.6, 1, -0.6], rotation: [0, 0, 0] }, // Top 
+    { position: [-0.6, 1, -0.6], rotation: [0, 0, 0] }, // Top 
+    { position: [0, -1, 0], rotation: [0, 0, 0] }, // Bottom 1
+    { position: [1, 0.6, 0.6], rotation: [0, 0,Math.PI/2 ] }, // Right 2
+    { position: [1, -0.6, -0.6], rotation: [0, 0,Math.PI/2 ] }, // Right 2
+    { position: [-1, 0, 0], rotation: [0, 0, Math.PI / 2] }, // Left 5
+    { position: [-1, 0.6, 0.6], rotation: [0, 0, Math.PI / 2] }, // Left 5
+    { position: [-1, 0.6, -0.6], rotation: [0, 0, Math.PI / 2] }, // Left 5
+    { position: [-1, -0.6, -0.6], rotation: [0, 0, Math.PI / 2] }, // Left 5
+    { position: [-1, -0.6, 0.6], rotation: [0, 0, Math.PI / 2] }, // Left 5
+    { position: [.6, 0.6, 1], rotation: [Math.PI/2, 0, 0] }, // Front 4
+    { position: [.6, -0.6, 1], rotation: [Math.PI/2, 0, 0] }, // Front 4
+    { position: [-.6, .6, 1], rotation: [Math.PI/2, 0, 0] }, // Front 4
+    { position: [-.6, -.6, 1], rotation: [Math.PI/2, 0, 0] }, // Front 4
+    { position: [0, 0, -1], rotation: [Math.PI/2, 0, 0] }, // Back
+    { position: [-0.6, 0.6, -1], rotation: [Math.PI/2, 0, 0] }, // Back
+    { position: [0.6, -0.6, -1], rotation: [Math.PI/2, 0, 0] }, // Back
   ];
   return positions[index];
 }
@@ -52,13 +66,18 @@ const ThreeD = () => {
     <Canvas>
       <ambientLight />
       <pointLight position={[10, 10, 10]} />
-      <Dice />
+      <Dice position={[2.5, 0, 0]} />
+      <Dice position={[-2.5, 0, 0]} />
+      <Dice position={[0, 2.5, 0]} />
+      <Dice position={[0, -2.5, 0]} />
+      <Dice position={[2.5, 2.5, 0]} />
+      <Dice position={[0, 0, 0]} />
       
       {/* Plane to catch shadows */}
-      <mesh receiveShadow position={[0, -1, 0]} rotation={[-Math.PI / 2, 0, 0]}>
+      {/* <mesh receiveShadow position={[0, -1, 0]} rotation={[-Math.PI / 2, 0, 0]}>
         <planeGeometry args={[10, 10]} />
         <meshStandardMaterial color="lightblue" />
-      </mesh>
+      </mesh> */}
     </Canvas>
   );
 };
